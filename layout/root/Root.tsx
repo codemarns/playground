fdbb1e97de8aa85adaf2cc73912f16f4c503d956
Marnien Cueba <marniencueba94@gmail.com>
2024-03-18 11:32:31 +0800

    delete unnecessary layout

--- layout/root/Root.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Root = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="min-h-screen w-full overflow-x-hidden overflow-y-auto bg-background">
-      {children}
-    </div>
-  );
-};
