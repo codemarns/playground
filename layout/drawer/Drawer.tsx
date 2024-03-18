8d3867c74719b3a06d3ab5a58b4bbc71fb642e76
Marnien Cueba <marniencueba94@gmail.com>
2024-03-18 11:32:44 +0800

    Drawer

--- layout/drawer/Drawer.tsx
+++ layout/drawer/Drawer.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Drawer = () => {
   return (
-    <aside className="fixed top-0 bottom-0 right-0 z-10  h-full w-64 bg-background"></aside>
+    <aside className="sticky top-14 z-10 h-[calc(100vh-56px)] w-64 bg-background text-sm"></aside>
   );
 };
