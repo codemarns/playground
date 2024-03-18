a6285c957b58706d03622ff2bce671712345c6db
Marnien Cueba <marniencueba94@gmail.com>
2024-03-18 11:33:20 +0800

    new hook

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,10 @@ export const useRemoveSlashInPath = (pathname: string) => {
   const newPathname = pathname.replace(/\//g, "");
   return newPathname;
 };
+
+// import { clsx } from 'clsx'
+// import { twMerge } from 'tailwind-merge'
+
+// export function cn(...inputs:any[]) {
+//   return twMerge(clsx(inputs))
+// }
