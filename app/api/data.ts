09843784e0a28556a61a2b7d3e5db5a2bf4571ba
Marnien Cueba <marniencueba94@gmail.com>
2024-03-18 11:31:30 +0800

    menu data

---
+++ app/api/data.ts
@@ -0,0 +1,182 @@
+export const data = [
+  {
+    id: "docs",
+    name: "docs",
+    route: "/docs",
+    leaf: [
+      {
+        id: "components",
+        group: "components",
+        leaf: [
+          {
+            id: "accordion",
+            name: "accordion",
+            route: "/docs/accordion",
+          },
+          {
+            id: "alert-dialog",
+            name: "alert dialog",
+            route: "/docs/alert-dialog",
+          },
+          {
+            id: "aspect-ratio",
+            name: "aspect ratio",
+            route: "/docs/aspect-ratio",
+          },
+          {
+            id: "avatar",
+            name: "avatar",
+            route: "/docs/avatar",
+          },
+          {
+            id: "button",
+            name: "button",
+            route: "/docs/button",
+          },
+          {
+            id: "checkbox",
+            name: "checkbox",
+            route: "/docs/checkbox",
+          },
+          {
+            id: "collapsible",
+            name: "collapsible",
+            route: "/docs/collapsible",
+          },
+          {
+            id: "context-menu",
+            name: "context menu",
+            route: "/docs/context-menu",
+          },
+          {
+            id: "card",
+            name: "card",
+            route: "/docs/card",
+          },
+          {
+            id: "dialog",
+            name: "dialog",
+            route: "/docs/dialog",
+          },
+          {
+            id: "dropdown-menu",
+            name: "dropdown menu",
+            route: "/docs/dropdown-menu",
+          },
+        ],
+      },
+      {
+        id: "reference",
+        group: "reference",
+        leaf: [
+          {
+            id: "form",
+            name: "form",
+            route: "/docs/form",
+          },
+          {
+            id: "hover-card",
+            name: "hover card",
+            route: "/docs/hover-card",
+          },
+          {
+            id: "menu-bar",
+            name: "menu bar",
+            route: "/docs/menu-bar",
+          },
+          {
+            id: "navigation-menu",
+            name: "navigation menu",
+            route: "/docs/navigation-menu",
+          },
+          {
+            id: "popover",
+            name: "popover",
+            route: "/docs/popover",
+          },
+          {
+            id: "progress",
+            name: "progress",
+            route: "/docs/progress",
+          },
+        ],
+      },
+      {
+        id: "utilities",
+        group: "utilities",
+        leaf: [
+          {
+            id: "radio-group",
+            name: "radio group",
+            route: "/docs/radio-group",
+          },
+          {
+            id: "scroll-area",
+            name: "scroll area",
+            route: "/docs/scroll-area",
+          },
+          {
+            id: "select",
+            name: "select",
+            route: "/docs/select",
+          },
+          {
+            id: "separator",
+            name: "separator",
+            route: "/docs/separator",
+          },
+          {
+            id: "slider",
+            name: "slider",
+            route: "/docs/slider",
+          },
+          {
+            id: "switch",
+            name: "switch",
+            route: "/docs/switch",
+          },
+          {
+            id: "tabs",
+            name: "tabs",
+            route: "/docs/tabs",
+          },
+          {
+            id: "toast",
+            name: "toast",
+            route: "/docs/toast",
+          },
+          {
+            id: "toggle",
+            name: "toggle",
+            route: "/docs/toggle",
+          },
+          {
+            id: "toggle-group",
+            name: "toggle group",
+            route: "/docs/toggle-group",
+          },
+          {
+            id: "toolbar",
+            name: "toolbar",
+            route: "/docs/toolbar",
+          },
+          {
+            id: "tooltip",
+            name: "tooltip",
+            route: "/docs/tooltip",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "guides",
+    name: "guides",
+    route: "/guides",
+  },
+  {
+    id: "design-system",
+    name: "design system",
+    route: "/design-system",
+  },
+];
